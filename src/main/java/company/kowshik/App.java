dcc9d5c699fb41bc4c7d76cb9b7d3291ec05a57e
shashank75 <gujjesaishashank@gmail.com>
2016-05-22 21:35:12 -0500

    first commit

---
+++ src/main/java/company/kowshik/App.java
@@ -0,0 +1,13 @@
+package company.kowshik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
